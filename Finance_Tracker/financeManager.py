d2ddd864a068637ce2c8446ebd48b82327194cdb
Andrew Cring <amcring1@gmail.com>
2023-07-31 12:19:26 -0700

    Adding in the categrories of the rows in the CSV files

--- Finance_Tracker/financeManager.py
+++ Finance_Tracker/financeManager.py
@@ -7,4 +7,7 @@ file = f"{MONTH}.csv"
 with open(file, mode='r') as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
+        date = row[0]
+        name = row[1]
+        amount = float(row[2])
         print(row)
