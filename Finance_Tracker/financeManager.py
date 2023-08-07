e51bb5a32b031e87405ce2c5bb56810c466c600c
Andrew Cring <amcring1@gmail.com>
2023-08-07 11:40:10 -0700

    Added some more details to track from the CSV and created the transaction variable.

--- Finance_Tracker/financeManager.py
+++ Finance_Tracker/financeManager.py
@@ -4,11 +4,15 @@ MONTH = 'may'
 
 file = f"{MONTH}.csv"
 
+transactions = []
+
 with open(file, mode='r') as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
         date = row[0]
         name = row[1]
+        category = 'other'
         amount = float(row[2])
         balance = float(row[3])
-        print(row)
+        transaction = ((date, name, category, amount))
+        print(transaction)
