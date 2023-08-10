0071e579fa895d601929b4fe2643461a204656d6
Andrew Cring <amcring1@gmail.com>
2023-08-10 06:13:25 -0700

    Adding in a category for the paycheck from traderjoes

--- Finance_Tracker/financeManager.py
+++ Finance_Tracker/financeManager.py
@@ -14,6 +14,8 @@ with open(file, mode='r') as csv_file:
         date = row[0]
         name = row[1]
         category = 'other'
+        if name == "Trader Joes Comp":
+            category = 'Paycheck'
         amount = float(row[2])
         balance = float(row[3])
         transaction = ((date, name, category, amount))
