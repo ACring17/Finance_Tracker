895b56b130b9c1d8efbf665be8be91af066460a7
Andrew Cring <amcring1@gmail.com>
2023-08-13 11:29:53 -0700

    Putting the final touches on the function to read the files

--- Finance_Tracker/financeManager.py
+++ Finance_Tracker/financeManager.py
@@ -10,18 +10,23 @@ running_balance = 0
 
 Subscriptions = {'Spotify USA', 'MYBESTBUY', 'MICROSOFT YEARLY'}
 
-with open(file, mode='r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        date = row[0]
-        name = row[1]
-        category = 'other'
-        if name == "Trader Joes Comp":
-            category = 'Paycheck'
-        if name == Subscriptions:
-            category = "Subscription"
-        amount = float(row[2])
-        balance = float(row[3])
-        running_balance += amount
-        transaction = ((date, name, category, amount))
-        print(transaction)
+def personalFin(file, Subscriptions):
+    with open(file, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            date = row[0]
+            name = row[1]
+            category = 'other'
+            if name == "Trader Joes Comp":
+                category = 'Paycheck'
+            if name == Subscriptions:
+                category = "Subscription"
+            amount = float(row[2])
+            balance = float(row[3])
+            running_balance += amount
+            transaction = ((date, name, category, amount))
+            transactions.append(transaction)
+        return transactions
+
+wks = sh.worksheet(f"{MONTH}")
+rows = personalFin(file,Subscriptions)
